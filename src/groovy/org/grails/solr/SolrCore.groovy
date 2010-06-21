6422d19510163350bb073d3f09119fc7988af497
Jan Rudert <rudi@lund.(none)>
2010-06-21 17:58:03 +0200

    encapsulates core information

---
+++ src/groovy/org/grails/solr/SolrCore.groovy
@@ -0,0 +1,14 @@
+package org.grails.solr
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rudi
+ * Date: 21.05.2010
+ * Time: 14:41:26
+ * To change this template use File | Settings | File Templates.
+ */
+class SolrCore {
+  String name
+  Class artefact
+  String url
+}
